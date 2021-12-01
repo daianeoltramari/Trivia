be88f878db5756ce51982362ddf8295d30187353
Daiane Oltramari <42157970+daianeoltramari@users.noreply.github.com>
2021-12-01 15:14:31 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+function enviar(){
+    document.getElementById('res').innerHTML = "<b>Gabarito: Questao 1 - letra A. Questão 2 - letra B. Questão 3 - letra C!</b>" 
+    //alert ('Resultado: Questao 1 - letra A. Questão 2 - letra B. Questão 3 - letra C!')
+}
